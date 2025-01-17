a11512bd5e01b202c97ff3a4ae0b4d3f7df62458
nafis200 <nafisahamed14@gmail.com>
2025-01-18 00:02:27 +0600

    first commit

---
+++ procedure/procedure1.md
@@ -0,0 +1,23 @@
+1. react-router dom from 6.28 docs
+2. react-hook-form install
+3. redux toolkit theke install two package. (redux toolkit/ react-redux)
+4. intall Ant design.
+5. Ant-design icon install from npm.
+
+6. work with sidebar Ant design
+
+7. Layout theke destructure korbo. ---> MainLayout.tsx
+
+8. createElement dirrect react theke import korbo. --> icons 
+
+9. MainLayout half asbe so set korte hobe.
+
+10. mainlayout header components e style 100% dite hobe.
+
+11. Add menu props Item.
+
+12. Now create routes
+
+13. create route and wrap routerProvider main.tsx.
+
+<!-- Create a route which we dirrectly shown as a sidebar -->
---
+++ src/components/layout/MainLayout.tsx
@@ -0,0 +1,85 @@
+import { Layout, Menu } from "antd";
+import { adminSidebarItems } from "../../routes/admin.routes";
+
+const { Header, Content, Sider } = Layout;
+
+
+// import { UploadOutlined, UserOutlined, VideoCameraOutlined } from '@ant-design/icons';
+// import { createElement } from "react";
+
+// const items:MenuProps['items'] = [
+//        {
+//          key:'Dashboard',
+//          label:'<NavLink>',
+//        },
+//        {
+//          key:'profile',
+//          label:'Profile',
+//          children: [
+//           {
+//             key:'Create Admin',
+//             label:'<NavLink>',
+//           },
+//           {
+//             key:'Create Student',
+//             label:'<NavLink>',
+//           },
+//          ]
+//        },
+
+// ]
+
+
+
+const MainLayout = () => {
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Sider
+        breakpoint="lg"
+        collapsedWidth="0"
+        onBreakpoint={(broken) => {
+          console.log(broken);
+        }}
+        onCollapse={(collapsed, type) => {
+          console.log(collapsed, type);
+        }}
+      >
+         <div
+          style={{
+            color: 'white',
+            height: '4rem',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <h1>PH Uni</h1>
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['4']}
+          items={adminSidebarItems}
+        />
+        
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: '' }} />
+        <Content style={{ margin: '24px 16px 0' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: '',
+              borderRadius: '',
+            }}
+          >
+            <h1>The main content should go here</h1>
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default MainLayout;
